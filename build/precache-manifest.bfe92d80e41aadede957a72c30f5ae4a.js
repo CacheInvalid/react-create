ab02671c5f7b37a94e56a76a197d0d894f4f622f
CacheInvalid <59955366+CacheInvalid@users.noreply.github.com>
2022-12-19 00:03:18 +0000

    Add files via upload

---
+++ build/precache-manifest.bfe92d80e41aadede957a72c30f5ae4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "47e3f0b48b87fb8440a8",
+    "url": "/static/css/main.bb1d5206.chunk.css"
+  },
+  {
+    "revision": "47e3f0b48b87fb8440a8",
+    "url": "/static/js/main.47e3f0b4.chunk.js"
+  },
+  {
+    "revision": "73c4a386b51143b368b7",
+    "url": "/static/js/1.73c4a386.chunk.js"
+  },
+  {
+    "revision": "78f33892cb4c1d0c2c86",
+    "url": "/static/css/2.530e365d.chunk.css"
+  },
+  {
+    "revision": "78f33892cb4c1d0c2c86",
+    "url": "/static/js/2.78f33892.chunk.js"
+  },
+  {
+    "revision": "c842cea4958dad719a98",
+    "url": "/static/js/runtime~main.c842cea4.js"
+  },
+  {
+    "revision": "bb48aef4ecacea58d2798c1ee172a2d5",
+    "url": "/index.html"
+  }
+];
